25c41021ab032b956c6a639f56bfc276220eefa3
MaheepSinghBhatia <maheepbha3751@gmail.com>
2022-11-09 22:01:19 -0600

    PE 6.23

---
+++ MaheepSinghBhatia_2200_PE6.23/src/Exercise06_23.java
@@ -0,0 +1,32 @@
+/******************************************************************
+ * 
+ *  @author Maheep Singh Bhatia
+ *  Date: 11/9/2022 [W]
+ *  
+ */
+import java.util.*;
+public class Exercise06_23 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a string: ");
+		String s = input.nextLine();
+		
+		System.out.print("Enter a character: ");
+		char c = input.nextLine().charAt(0);
+		
+		int count = count(s, c);
+		System.out.println("The number of " + c + " in " + s + " is " + count);
+		
+		input.close();
+	}
+	public static int count(String str, char ch) {
+		int count = 0;
+		for(int i = 0; i < str.length(); i++) {
+			if(str.charAt(i) == ch) {
+			count++;	
+			}
+		}
+		return count;
+	}
+}
